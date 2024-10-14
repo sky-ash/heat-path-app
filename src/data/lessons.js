444fc005559f4f7f951379a7f915ff6532cce67e
sky-ash <sky-ash-dev@proton.me>
2024-10-14 16:08:52 +0200

    text-based example data for 4 lectures

--- src/data/lessons.js
+++ src/data/lessons.js
@@ -20,6 +20,72 @@ export const lessons = [
         },
       ],
     },
-    // Repeat for other lectures, up to Lecture 4
+    {
+      id: 2,
+      title: "Lecture 2",
+      cards: [
+        "2-1 x",
+        "2-2 xx",
+        "2-3 xxx",
+      ],
+      quiz: [
+        {
+          question: "q1",
+          options: ["a1", "a2", "a3 (r)"],
+          answer: "a3 (r)",
+        },
+        {
+          question: "question 2.",
+          options: ["Richtige Antwort", "andere antwort"],
+          answer: "Richtige Antwort",
+        },
+        {
+          question: "and a 3rd",
+          options: ["correct", "wrong", "wrong again", "nuh-uh"],
+          answer: "correct",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Lecture 3",
+      cards: [
+        "lecture 3 card 1",
+        "L3 C2",
+        "L3 C3",
+        "L3 C4",
+      ],
+      quiz: [
+        {
+          question: "only one question?",
+          options: ["this is the correct answer", "nah", "nope", "nada"],
+          answer: "this is the correct answer",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Lecture 4",
+      cards: [
+        "LAST LECTURE! This is a card",
+        "lorem ipsum",
+        "ipsum schmipsum",
+        "here, more cards to click through: 4-4",
+        "ABC",
+        "XYZ",
+      ],
+      quiz: [
+        {
+          question: "This is the first question of the last lecture.",
+          options: ["Y", "N"],
+          answer: "Y",
+        },
+        {
+          question: "How many lectures were there?",
+          options: ["1", "2", "3", "4"],
+          answer: "4",
+        },
+      ],
+    },
   ];
   
